6b2066b6a8ffc6a1fc03d6f739d74447498e2430
coding <coding@Jamals-MacBook-Air.local>
2023-07-24 20:24:42 +0100

    JS and HTML

---
+++ Javascript/Lessons/Lesson1.js
@@ -0,0 +1,19 @@
+//this is a single line comment
+
+/*
+this 
+will be
+a
+multi
+line comment*/
+
+console.log("Hello you have executedthe lesson 1 file.");
+
+console.log("hello world!");
+
+var greeting = "Salaam"
+var myName = "Jamal";
+
+
+
+console.log(greeting+" "+myName);

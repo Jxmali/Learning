3c7b74bec2f1a266a523cf272c1fefc0ab974002
coding <coding@Jamals-MacBook-Air.local>
2023-08-06 14:31:09 +0100

    conditional statements

---
+++ Javascript/Lessons/Lesson3.js
@@ -0,0 +1,35 @@
+//Conditional Statements
+
+/*
+if (condition){
+    doThis
+}else if(conditon){
+    doThis
+}
+else{
+    doThis
+}
+*/
+
+// single = is for setting up a variable
+// double == 1 "1"
+// triple === 1 "1"
+
+// AND gate &&
+// OR gate || 
+
+let name = "Jamal"
+var age = 27
+let array = ["apples", "milk", "cheese"]
+
+console.log(array)
+
+if (name === "Jamal" && age === 25){
+    console.log("Hello Jamal, welcome back.")
+    array[0] = "pears"
+}else{
+    console.log("Idk you")
+}
+
+console.log(array)
+
---
+++ Javascript/Lessons/Lesson4.js
@@ -0,0 +1,36 @@
+// includes ( )
+
+var array = [1,2,3,4,5,6,"Jamal"]
+
+if (array.includes("z")){
+    console.log("array has that item.")
+}
+
+var day = "TUESDAY"
+
+switch(day.toLowerCase()){
+    case "monday":
+        console.log(`Today is ${day}.`)
+        break;
+    case "tuesday":
+        console.log(`Today is ${day}.`)
+        break;
+    case "wednesday":
+        console.log(`Today is ${day}.`)
+        break;
+    case "thursday":
+        console.log(`Today is ${day}.`)
+        break;
+    case "friday":
+        console.log(`Today is ${day}.`)
+        break;
+    case "saturday":
+        console.log(`Today is ${day}.`)
+        break;
+    case "sunday":
+        console.log(`Today is ${day}.`)
+        break;
+    default:
+        console.log("not possible")
+        break;
+}
